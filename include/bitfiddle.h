3cbfcce45ddb5c5c63576acc0208b136118dfb73
Marc Brinkmann <git@marcbrinkmann.de>
2010-07-24 20:53:31 +0200

    Added bit-fiddling headers.

---
+++ include/bitfiddle.h
@@ -0,0 +1,17 @@
+#ifndef BITFIDDLE_H
+#define BITFIDDLE_H 
+
+#define BIT(n) (1 << (n))
+
+#define SET_BITS(var, mask) (var |= (mask))
+#define CLEAR_BITS(var, mask) (var &= (mask))
+
+#define SET_BIT(var, n) (SET_BITS(BIT(n)))
+#define CLEAR_BIT(var, n) (CLEAR_BITS(BIT(n)))
+
+#define BITS_SET(var, mask) (((var) & (mask)) == (mask))
+#define BITS_CLEARED(var, mask) ((~(var) & (mask)) == (mask))
+
+#define GET_BIT(var, n) ((var) & BIT(n) >> (n))
+
+#endif /* BITFIDDLE_H */
