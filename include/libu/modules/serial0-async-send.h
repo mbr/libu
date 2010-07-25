90ad91d00faf02768d90a9eaad1c8315cf3dfece
Marc Brinkmann <git@marcbrinkmann.de>
2010-07-25 21:31:26 +0200

    Added serial0-async-send module.

---
+++ include/libu/modules/serial0-async-send.h
@@ -0,0 +1,17 @@
+#ifndef SERIAL0_ASYNC_SEND_H
+#define SERIAL0_ASYNC_SEND_H 
+
+#include <libu/struct/ringbuf.h>
+
+extern ringbuf_t _serial0_async_send_sbuf;
+
+inline static void serial0_async_putc(char c) {
+	ringbuf_putc(&_serial0_async_send_sbuf, c);
+
+	/* enable Data Register Empty Interrupt */
+	SET_BIT(UCSR0B, UDRIE0);
+}
+
+void serial0_async_send_init(size_t bufsize);
+
+#endif /* SERIAL0-ASYNC-SEND_H */
---
+++ src/modules/serial0-async-send.c
@@ -0,0 +1,27 @@
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+#include <libu/bitfiddle.h>
+#include <libu/avr/serial.h>
+
+#include <libu/modules/serial0-async-send.h>
+
+ringbuf_t _serial0_async_send_sbuf;
+
+/* FIXME: this may not be support on atmega 128 */
+SIGNAL(USART_UDRE_vect) {
+	/* interrupts are turned off, and the data register is empty. */
+	if (ringbuf_available(&_serial0_async_send_sbuf)) {
+		/* data available, send */
+		serial0_send_byte(ringbuf_getc(&_serial0_async_send_sbuf));
+	} else {
+		/* no data available - clear interrupt handler. will
+		 * be set again if more data to send is added. */
+		CLEAR_BIT(UCSR0B, UDRIE0);
+	}
+}
+
+void serial0_async_send_init(size_t bufsize) {
+	char *buffer = malloc(sizeof(char) * bufsize);
+	ringbuf_init(&_serial0_async_send_sbuf, buffer, bufsize, NULL, NULL);
+}
