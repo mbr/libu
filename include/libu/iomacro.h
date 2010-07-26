b079df499e3653cf3f0188be3ee1d4466ab0c07b
Marc Brinkmann <git@marcbrinkmann.de>
2010-07-26 13:08:18 +0200

    Fixed wrong name in function.

--- include/libu/iomacro.h
+++ include/libu/iomacro.h
@@ -1,7 +1,7 @@
 #ifndef IOMACRO_H
 #define IOMACRO_H 
 
-#define DECLARE_SEND_STR(name) void name##_send_str(const char *s)
+#define DECLARE_SEND_STR(name) void name##_blocking_send_str(const char *s)
 #define DEFINE_SEND_STR(name) DECLARE_SEND_STR(name) { while(*s) { name##_block_until_ready(); name##_putc(*(s++)); } }
 
 #define DECLARE_SEND_DATA(name) void name##_blocking_send_data(const char *s, size_t len)
