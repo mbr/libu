dba6abf24d926ccc53358b3fe52e45a5d039eb4d
Marc Brinkmann <git@marcbrinkmann.de>
2010-07-26 13:01:42 +0200

    Reordered serial.h.

--- include/libu/avr/serial.h
+++ include/libu/avr/serial.h
@@ -5,18 +5,6 @@
 #include <inttypes.h>
 #include <string.h>
 
-/* block, sending a string string */
-void serial0_blocking_send_str(const char *s);
-void serial0_blocking_send_data(const char *s, size_t len);
-
-/* setup methods */
-void serial0_set_baud_rate(const uint32_t baud_rate);
-
-/* inline methods */
-inline static void serial0_putc(char c) {
-	UDR0 = c;
-}
-
 inline static int serial0_ready_to_send() {
 	return UCSR0A & (1 << UDRE0);
 }
@@ -24,5 +12,21 @@ inline static int serial0_ready_to_send() {
 inline static void serial0_block_until_ready() {
 	while(!serial0_ready_to_send());
 }
+
+/* inline methods */
+inline static void serial0_putc(char c) {
+	UDR0 = c;
+}
+
+/* block, sending a string string */
+void serial0_blocking_send_str(const char *s);
+void serial0_blocking_send_data(const char *s, size_t len);
+inline static void serial0_blocking_send(const char c) {
+	serial0_block_until_ready();
+	serial0_putc(c);
+}
+
+/* setup methods */
+void serial0_set_baud_rate(const uint32_t baud_rate);
 
 #endif /* SERIAL_H */
